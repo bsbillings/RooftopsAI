54b53f6f89e344fc1163025ba59b5747208b7a7f
Steele Billings <bsbillings@gmail.com>
2024-01-21 18:46:15 -0600

    Initial Commit

---
+++ RooftopsAI/RooftopsAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RooftopsAIApp.swift
+//  RooftopsAI
+//
+//  Created by Steele Billings on 1/21/24.
+//
+
+import SwiftUI
+
+@main
+struct RooftopsAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
